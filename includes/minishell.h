7eb7ced06bc8062c2a7d330f3197a7aabff4a3fe
Othmane Bounri <obounri@e1r5p3.1337.ma>
2021-11-02 17:23:51 +0100

    prompt

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,8 +1,17 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+
+typedef struct s_options
+{
+    char    *user;
+    char    *input;
+    int     success;
+}   t_options;
+
 
 #endif
--- main.c
+++ main.c
@@ -6,23 +6,43 @@
 /*   By: obounri <obounri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 17:35:32 by obounri           #+#    #+#             */
-/*   Updated: 2021/11/01 18:35:05 by obounri          ###   ########.fr       */
+/*   Updated: 2021/11/02 17:22:59 by obounri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
+void    prompt(t_options *opts)
+{
+	if (opts->success)
+		printf("\033[0;32m");
+	else
+		printf("\033[0;31m");
+	printf("➤ ");
+	printf("\033[0m");
+	printf(" %s ~ ", opts->user);
+	opts->input = readline("");
+}
+
+// void		catch(int sig)
+// {
+// 	printf("\n");
+// }
+
 int main(void)
 {
-    char *line;
-    char *user;
-    
-    user = readline("Enter user name for prompt:");
-    while (1)
-    {
-        printf("%s ==> ", user);
-        line = readline("");
-        printf("%s\n", line);
-    }
-    return (0);
+	t_options	opts;
+	
+	opts.user = readline("Enter user name for prompt: ");
+	opts.success = 1;
+	while (1)
+	{
+		// signal(SIGINT, &catch);
+		prompt(&opts);
+		if (opts.success)
+			opts.success = 0;
+		else
+			opts.success = 1;
+	}
+	return (0);
 }
